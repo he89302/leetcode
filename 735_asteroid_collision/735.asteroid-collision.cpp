89d873d06a4a00f15e9fff4fd5246d2b5c4939f2
Wen_po Huang <he89302@gmail.com>
2023-03-04 16:16:25 +0800

    Solved 735_asteroid_collision using stack.
    (cherry picked from commit 8128ffad33e619a4d590d014fb04d169a0cee7a6)

---
+++ 735_asteroid_collision/735.asteroid-collision.cpp
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode id=735 lang=cpp
+ *
+ * [735] Asteroid Collision
+ *
+ * https://leetcode.com/problems/asteroid-collision/description/
+ *
+ * algorithms
+ * Medium (44.36%)
+ * Likes:    4711
+ * Dislikes: 417
+ * Total Accepted:    258.3K
+ * Total Submissions: 581.5K
+ * Testcase Example:  '[5,10,-5]'
+ *
+ * We are given an array asteroids of integers representing asteroids in a
+ * row.
+ * 
+ * For each asteroid, the absolute value represents its size, and the sign
+ * represents its direction (positive meaning right, negative meaning left).
+ * Each asteroid moves at the same speed.
+ * 
+ * Find out the state of the asteroids after all collisions. If two asteroids
+ * meet, the smaller one will explode. If both are the same size, both will
+ * explode. Two asteroids moving in the same direction will never meet.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: asteroids = [5,10,-5]
+ * Output: [5,10]
+ * Explanation: The 10 and -5 collide resulting in 10. The 5 and 10 never
+ * collide.
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: asteroids = [8,-8]
+ * Output: []
+ * Explanation: The 8 and -8 collide exploding each other.
+ * 
+ * 
+ * Example 3:
+ * 
+ * Input: asteroids = [10,2,-5]
+ * Output: [10]
+ * Explanation: The 2 and -5 collide resulting in -5. The 10 and -5 collide
+ * resulting in 10.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 2 <= asteroids.length <= 10^4
+ * -1000 <= asteroids[i] <= 1000
+ * asteroids[i] != 0
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> res;
+        // res.push_back(asteroids[0]);
+
+        for (int asteroid : asteroids)
+        {
+            while (!res.empty() && asteroid < 0 && res.back() > 0 )
+            {
+                int temp = res.back(); res.pop_back();
+                int left = temp + asteroid;
+                if(left == 0) asteroid = 0;
+                if(left > 0) asteroid = temp;
+            }
+
+            if(asteroid != 0) res.push_back(asteroid);
+        }
+        return res;
+    }
+};
+// @lc code=end
