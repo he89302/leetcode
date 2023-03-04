e8b3c7c899343bc598f23177c0bb62cabaeb307d
Wen_po Huang <he89302@gmail.com>
2023-03-04 22:30:06 +0800

    Solved 215 using priority queue.

--- 215_kth_largest_element_in_an_array/215.kth-largest-element-in-an-array.cpp
+++ 215_kth_largest_element_in_an_array/215.kth-largest-element-in-an-array.cpp
@@ -43,9 +43,12 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        return nums[n - k];
+        priority_queue<int> pq(nums.begin(), nums.end());
+        for(int i = 0; i < k - 1; i++) {
+            pq.pop();
+        }
+
+        return pq.top();
     }
 };
 // @lc code=end
