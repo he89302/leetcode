85af05714d6e5046f7ebeaadbf6f66fa5dd3afb6
Wen_po Huang <he89302@gmail.com>
2023-03-12 21:58:16 +0800

    Sovled by using dynamic program.

--- 53_maximum_subarray/53.maximum-subarray.cpp
+++ 53_maximum_subarray/53.maximum-subarray.cpp
@@ -56,14 +56,14 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int currentSum = 0, bestSum = nums[0];
+         vector<int> dp(nums.size());
+        dp[0] = nums[0];
 
-        for(int i = 0; i < nums.size(); i++) {
-            currentSum = max(nums[i], nums[i] + currentSum);
-            bestSum = max(currentSum, bestSum);
+        for(int i = 1; i < nums.size(); i++) {
+            dp[i] = max(nums[i], nums[i] + dp[i - 1]);
         }
 
-        return bestSum;
+        return *max_element(dp.begin(), dp.end());
     }
 
 };
