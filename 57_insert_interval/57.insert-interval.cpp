447476bb8e7b7b26cadcadbd1f1b1f2083da46b1
Wen_po Huang <he89302@gmail.com>
2023-03-14 01:57:17 +0800

    Carding and review leetcode 57. using array and while to collect newInterval

---
+++ 57_insert_interval/57.insert-interval.cpp
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode id=57 lang=cpp
+ *
+ * [57] Insert Interval
+ *
+ * https://leetcode.com/problems/insert-interval/description/
+ *
+ * algorithms
+ * Medium (39.00%)
+ * Likes:    8034
+ * Dislikes: 556
+ * Total Accepted:    736.9K
+ * Total Submissions: 1.9M
+ * Testcase Example:  '[[1,3],[6,9]]\n[2,5]'
+ *
+ * You are given an array of non-overlapping intervals intervals where
+ * intervals[i] = [starti, endi] represent the start and the end of the i^th
+ * interval and intervals is sorted in ascending order by starti. You are also
+ * given an interval newInterval = [start, end] that represents the start and
+ * end of another interval.
+ * 
+ * Insert newInterval into intervals such that intervals is still sorted in
+ * ascending order by starti and intervals still does not have any overlapping
+ * intervals (merge overlapping intervals if necessary).
+ * 
+ * Return intervals after the insertion.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+ * Output: [[1,5],[6,9]]
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+ * Output: [[1,2],[3,10],[12,16]]
+ * Explanation: Because the new interval [4,8] overlaps with
+ * [3,5],[6,7],[8,10].
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 0 <= intervals.length <= 10^4
+ * intervals[i].length == 2
+ * 0 <= starti <= endi <= 10^5
+ * intervals is sorted by starti in ascending order.
+ * newInterval.length == 2
+ * 0 <= start <= end <= 10^5
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int i = 0;
+        vector<vector<int>> result;
+
+        while(i < intervals.size() && intervals[i].back() < newInterval[0]) {
+            result.push_back(intervals[i]);
+            i++;
+        }
+
+        while(i < intervals.size() && !(intervals[i].front() > newInterval.back())) {
+            newInterval.front() = min(intervals[i].front(), newInterval.front());
+            newInterval.back() = max(intervals[i].back(), newInterval.back());
+            i++;
+        }
+
+        result.push_back(newInterval);
+        while(i < intervals.size()) {
+            result.push_back(intervals[i]);
+            i++;
+        }
+
+        return result;
+    }
+};
+// @lc code=end
+
