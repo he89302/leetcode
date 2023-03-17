9c7ce63e5b7e84909ccc9e5a77e5818d092a3f0b
Wen_po Huang <he89302@gmail.com>
2023-03-18 04:57:29 +0800

    Carding leetcode 973, using priority queue to solved.

---
+++ 973_stamping_the_sequence/973.stamping-the-sequence.cpp
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode id=1014 lang=cpp
+ *
+ * [973] K Closest Points to Origin
+ *
+ * https://leetcode.com/problems/k-closest-points-to-origin/description/
+ *
+ * algorithms
+ * Medium (65.74%)
+ * Likes:    7232
+ * Dislikes: 260
+ * Total Accepted:    956K
+ * Total Submissions: 1.5M
+ * Testcase Example:  '[[1,3],[-2,2]]\n1'
+ *
+ * Given an array of points where points[i] = [xi, yi] represents a point on
+ * the X-Y plane and an integer k, return the k closest points to the origin
+ * (0, 0).
+ * 
+ * The distance between two points on the X-Y plane is the Euclidean distance
+ * (i.e., √(x1 - x2)^2 + (y1 - y2)^2).
+ * 
+ * You may return the answer in any order. The answer is guaranteed to be
+ * unique (except for the order that it is in).
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: points = [[1,3],[-2,2]], k = 1
+ * Output: [[-2,2]]
+ * Explanation:
+ * The distance between (1, 3) and the origin is sqrt(10).
+ * The distance between (-2, 2) and the origin is sqrt(8).
+ * Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+ * We only want the closest k = 1 points from the origin, so the answer is just
+ * [[-2,2]].
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: points = [[3,3],[5,-1],[-2,4]], k = 2
+ * Output: [[3,3],[-2,4]]
+ * Explanation: The answer [[-2,4],[3,3]] would also be accepted.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= k <= points.length <= 10^4
+ * -10^4 < xi, yi < 10^4
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        priority_queue<pair<long, int>> pq;
+        vector<vector<int>> res;
+
+        for(int i = 0; i < points.size(); i++) {
+            long dis = points[i][0] * points[i][0] + points[i][1] * points[i][1];
+            pq.push({dis, i});
+            if(pq.size() > k)
+                pq.pop();
+        }
+
+        while(!pq.empty()) {
+            int index = pq.top().second;
+            res.push_back(points[index]);
+            pq.pop();
+        }
+
+        return res;
+    }
+};
+// @lc code=end
+
---
+++ 973_stamping_the_sequence/perspectived.md
@@ -0,0 +1,5 @@
+### calc point to points as distance
+> using priority queue to decide distance to (0, 0) 的距離長短排序 (升冪)
+> 並且如果pq size 超出k value 的話就pop 最後element; 也就是k 個距離中距離最大的
+> 最後將pq 倒出成response
+
