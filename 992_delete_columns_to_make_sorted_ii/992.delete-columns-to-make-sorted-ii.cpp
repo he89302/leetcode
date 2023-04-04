1b96a9f2a81667219b6445f3dcdfa4e53506de97
Wen_po Huang <he89302@gmail.com>
2023-04-04 11:28:07 +0800

    WIP leetcode 992, not known the method work.

--- 992_delete_columns_to_make_sorted_ii/992.delete-columns-to-make-sorted-ii.cpp
+++ 992_delete_columns_to_make_sorted_ii/992.delete-columns-to-make-sorted-ii.cpp
@@ -56,7 +56,22 @@
 class Solution {
 public:
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        
+        return getMostK(nums, k) - getMostK(nums, k - 1);
+    }
+
+private:
+    int getMostK(vector<int> &nums, int k) {
+        int i = 0, res = 0;
+        unordered_map<int, int> count;
+        for (int j = 0; j < nums.size(); ++j) {
+            if (!count[nums[j]]++) k--;
+            while (k < 0) {
+                if (!--count[nums[i]]) k++;
+                i++;
+            }
+            res += j - i + 1;
+        }
+        return res;
     }
 };
 // @lc code=end
---
+++ 992_delete_columns_to_make_sorted_ii/perspective.md
@@ -0,0 +1,8 @@
+### exactly(K) = atMost(K) - atMost(K-1)
+
+ * Input: nums = [1,2,1,2,3], k = 2
+ * Output: 7
+ * Explanation: Subarrays formed with exactly 2 different integers: [1,2],
+ * [2,1], [1,2], [2,3], [1,2,1], [2,1,2], [1,2,1,2]
+
+ 
